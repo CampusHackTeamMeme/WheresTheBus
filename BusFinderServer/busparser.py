a9b4bdbf99c1c0205f89c73ceb159ffd41005014
superdyoll <superdyoll@gmail.com>
2018-03-10 16:32:20 +0000

    Now creates DB

--- BusFinderServer/busparser.py
+++ BusFinderServer/busparser.py
@@ -1,15 +1,70 @@
-import requests
+import argparse
+import sqlite3
 
-# from xmljson import badgerfish as bf
-# from xml.etree.ElementTree import fromstring
+import os
 
-url = 'http://data.southampton.ac.uk/dumps/bus-info/2018-03-04/routes.json'
-
-r = requests.get(url)
-
-if r.status_code == 200:
-    Jdata = r.json()
+ROUTES_URL = 'http://data.southampton.ac.uk/dumps/bus-info/2018-03-04/routes.json'
+STOPS_URL = 'http://data.southampton.ac.uk/dumps/bus-info/2018-03-04/stops.json'
 
 
-# get xml
-# temp_json=bf.data(fromstring(temp_xml))
+def create_db(conn):
+    c = conn.cursor()
+
+    # Create history table
+    c.execute('''
+    CREATE TABLE stops(
+        stop_id TEXT PRIMARY KEY NOT NULL,
+        desc TEXT,
+        lat REAL,
+        lon REAL
+    )''')
+    c.execute('''
+    CREATE TABLE routes(
+        route_id TEXT PRIMARY KEY NOT NULL,
+        desc TEXT,
+        operator TEXT,
+        direction TEXT,
+        service TEXT
+    )''')
+    c.execute('''
+    CREATE TABLE routes_stops(
+        route_id TEXT NOT NULL,
+        stop_id TEXT NOT NULL,
+        PRIMARY KEY (route_id, stop_id),
+        FOREIGN KEY (route_id) REFERENCES routes (route_id)
+          ON DELETE CASCADE ON UPDATE NO ACTION,
+        FOREIGN KEY (stop_id) REFERENCES stops (stop_id)
+          ON DELETE CASCADE ON UPDATE NO ACTION
+    )''')
+
+    # Save (commit) the changes
+    conn.commit()
+
+
+def delete_db(conn):
+    c = conn.cursor()
+    c.execute('''DROP TABLE history''')
+    c.execute('''DROP TABLE names''')
+    c.execute('''DROP TABLE notes''')
+    conn.commit()
+
+
+def main(db_name, should_create_db):
+    exists = os.path.isfile(db_name)
+    with sqlite3.connect(db_name) as conn:
+        # Init the DB
+        if not exists:
+            create_db(conn)
+            print("Database file '{}' didn't exist, created.".format(db_name))
+        elif should_create_db:
+            delete_db(conn)
+            create_db(conn)
+            print("Deleted database and recreated.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--create_db", action='store_true')
+    parser.add_argument("--database", "-d", default="bus_finder.db")
+    args = parser.parse_args()
+    main(args.database, args.create_db)
