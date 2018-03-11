9f99d563722be6fcf6d90386796a53b07d3d46f2
Josh <joshuadp117@gmail.com>
2018-03-11 02:44:12 +0000

    serviceStops endpoint

--- BusFinderServer/main.py
+++ BusFinderServer/main.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 
 from busstops import busStops
 from routeinfo import routeInfo
+from servicestops import serviceStops
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,5 +12,6 @@ DBfile = 'bus_finder.db'
 
 api.add_resource(busStops, '/api/busstops', resource_class_args=(DBfile,))
 api.add_resource(routeInfo, '/api/routeinfo', resource_class_args=(DBfile,))
+api.add_resource(serviceStops, '/api/servicestops', resource_class_args=(DBfile,))
 
 app.run(host='0.0.0.0', port=8080, debug=True)
