2f36d79deb2790107c5729a475fa59629094bbd8
tconstantin <ct2u16@soton.ac.uk>
2018-03-11 11:25:26 +0000

    Add Timetable request

---
+++ BusFinderApp/app/src/main/java/meme/wheresthebus/comms/data/TimetableItem.java
@@ -0,0 +1,11 @@
+package meme.wheresthebus.comms.data;
+
+/**
+ * Created by costin on 11/03/2018.
+ */
+
+public class TimetableItem {
+    public String service;
+    public String timeto;
+    public String dest;
+}
