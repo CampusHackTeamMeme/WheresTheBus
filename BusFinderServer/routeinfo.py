bdeaa7805d946f77d6950cb77a83958bff81fecb
Josh <joshuadp117@gmail.com>
2018-03-18 00:46:42 +0000

    argument parser + faster query time

--- BusFinderServer/routeinfo.py
+++ BusFinderServer/routeinfo.py
@@ -1,24 +1,22 @@
-from flask_restful import Resource
-from flask import request
 import sqlite3 as sql
 
+from flask_restful import Resource, reqparse
 
 class RouteInfo(Resource):
     def __init__(self, file):
         self.DBfile = file
 
+        self.getParser = reqparse.RequestParser(bundle_errors=True)
+        self.getParser.add_argument('stop', required=True, location='form')
+
     def get(self):
-        r = request.args.to_dict()
-        print(r)
+        r = self.getParser.parse_args()
 
         conn = sql.connect(self.DBfile)
         c = conn.cursor()
 
-        toSend = {}
-
-
         query = c.execute(
-            '''SELECT routes.string_bus 
+            '''SELECT DISTINCT routes.string_bus 
             FROM routes_stops
             INNER JOIN routes ON routes_stops.route_id = routes.route_id
             WHERE routes_stops.stop_id = ?
@@ -27,8 +25,5 @@ class RouteInfo(Resource):
         data = []
         for i in query.fetchall():
             data.append(i[0])
-        data = list(set(data))
 
-        toSend['stop'] = data
-
-        return toSend, 200
+        return {'stop': data}, 200
